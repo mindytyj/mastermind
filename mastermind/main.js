9f1691b283ce9da8abdcadb2c452fc50ea1b3658
mindytyj <derpbritz@gmail.com>
2023-05-18 19:03:21 +0800

    Ver 1: Initial HTML, CSS and JS Set-up

--- mastermind/main.js
+++ mastermind/main.js
@@ -1,1 +1,54 @@
+/*----- constants -----*/
 
+/*----- state variables -----*/
+const game = {
+  board: [],
+  turn: null,
+  winner: null,
+  message: "",
+};
+
+/*----- cached elements  -----*/
+const gameBoard = document.querySelector("#gameBoard");
+
+const checkSelection = document.querySelector("#check");
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+function render() {
+  renderBoard();
+  renderMessage();
+}
+
+function renderBoard() {
+  gameBoard.innerHTML = "";
+}
+
+function init() {
+  game.board = [
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+  ];
+
+  turn = "player";
+
+  winner = null;
+
+  render();
+}
+
+function main() {
+  init();
+  render();
+}
+
+main();
