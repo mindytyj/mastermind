d090bf049f0c85cac3eacbc5f94f76cfa32a0a57
mindytyj <derpbritz@gmail.com>
2023-05-19 23:26:45 +0800

    Ver 3.1: Amended Game State Variables, Initialisation and Rendering
    Amended:
    1. Game board to split into main and side peg boards and included player selection
    2. Initialisation to include main and side peg boards and player selection
    3. Render board and message

--- mastermind/main.js
+++ mastermind/main.js
@@ -3,31 +3,63 @@
 2. Player to select the colour pegs
 2.1 Selected colour pegs will reflect on the main peg board
 2.2 Player to click on the check button once 4 coloured pegs are selected
-2.3 Check button to push the selected colours into board array and check for winning conditions
+2.3 Check button to push the selected colours into player selection board array and check for winning conditions
 3. If player's coloured pegs includes the colour but is in the wrong position, side peg to turn black.
 3.1 If player's coloured pegs is the right colour in the right position, side peg to turn red
-4. Player to continue the game until winning condition is met.
+4. Player to continue the game until winning condition is met or guess is more than 10.
 5. Once player wins/lose, output div to unhide gameMessage class and winning message is reflected to player
 6. Player to click on reset game button to reset the game board, game message and secret code.
 */
 
 /*----- constants -----*/
+const COLOURS = ["red", "yellow", "blue", "green"];
 
 /*----- state variables -----*/
 const game = {
   secretCode: [],
-  board: [],
+  boardMainPeg: [],
+  boardSidePeg: [],
+  playerSelection: [],
   message: "",
 };
 
 /*----- cached elements  -----*/
-const gameBoard = document.querySelector("#gameBoard");
+const gameMainBoard = document.querySelectorAll(".mainPegs > div");
+const gameSideBoard = document.querySelectorAll(".sidePegs > div");
+const gameMessage = document.querySelector("#gameMessage");
 
 const checkSelection = document.querySelector("#check");
 
 /*----- event listeners -----*/
 
+// redPegSelection.addEventListener("click", handleRedPeg);
+// yellowPegSelection.addEventListener("click", handleYellowPeg);
+// bluePegSelection.addEventListener("click", handleBluePeg);
+// greenPegSelection.addEventListener("click", handleGreenPeg);
+
 /*----- functions -----*/
+
+// function handleRedPeg(event) {
+//   let selectedPeg = renderMainPegs();
+//   selectedPeg.style.color = "red";
+// }
+
+const renderBoard = () => {
+  gameMainBoard.innerHTML = "";
+  gameSideBoard.innerHTML = "";
+
+  gameMainBoard.forEach((mainPeg) => {
+    mainPeg.style.color = "white";
+  });
+
+  gameSideBoard.forEach((sidePeg) => {
+    sidePeg.style.color = "white";
+  });
+};
+
+const renderMessage = () => {
+  gameMessage.innerText = game.message;
+};
 
 const render = () => {
   renderBoard();
@@ -37,7 +69,7 @@ const render = () => {
 const init = () => {
   game.secretCode = [null, null, null, null];
 
-  game.board = [
+  game.boardMainPeg = [
     [null, null, null, null],
     [null, null, null, null],
     [null, null, null, null],
@@ -49,6 +81,21 @@ const init = () => {
     [null, null, null, null],
     [null, null, null, null],
   ];
+
+  game.boardSidePeg = [
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+  ];
+
+  game.playerSelection = [null, null, null, null];
 };
 
 const generateRandomCode = () => {
@@ -67,6 +114,7 @@ const computerSecretCode = () => {
 const main = () => {
   init();
   computerSecretCode();
+  render();
 };
 
 main();
